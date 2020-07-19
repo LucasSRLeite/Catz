f9e36143debe7660967a274fefdca6ee8d4f819b
Lucas Leite <lucas.leite@protonmail.com>
2020-07-19 16:31:11 -0300

    Create extension for collectionView in order to facilitate the bind

---
+++ Catz/Extensions/UICollectionView+CombineDataSources.swift
@@ -0,0 +1,27 @@
+//
+//  UICollectionView+CombineDataSources.swift
+//  Catz
+//
+//  Created by Lucas Leite on 17/07/20.
+//  Copyright © 2020 Lucas Leite. All rights reserved.
+//
+
+import Combine
+import CombineDataSources
+import UIKit
+
+extension UICollectionView {
+    func itemsSubscriber<CellType, Items>(cellType: CellType.Type, cellConfig: @escaping CollectionViewItemsController<[Items]>.CellConfig<Items.Element, CellType>) -> AnySubscriber<Items, Never> where CellType: UICollectionViewCell, Items: RandomAccessCollection, Items: Equatable {
+        itemsSubscriber(cellIdentifier: String(describing: cellType), cellType: cellType, cellConfig: cellConfig)
+    }
+
+    func register(cellClass: AnyClass) {
+        register(cellClass, forCellWithReuseIdentifier: String(describing: cellClass))
+    }
+}
+
+extension Publisher where Output: Collection {
+    func compactMapMany<Result>(_ transform: @escaping (Output.Element) -> Result?) -> Publishers.Map<Self, [Result]> {
+        map { $0.compactMap(transform) }
+    }
+}
