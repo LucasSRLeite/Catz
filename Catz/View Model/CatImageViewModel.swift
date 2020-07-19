68e55f566adc3f53993577a5e5741ce1a73084e6
Lucas Leite <lucas.leite@protonmail.com>
2020-07-19 16:29:58 -0300

    Implement ViewModels for cats list and catImage

---
+++ Catz/View Model/CatImageViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  CatViewModel.swift
+//  Catz
+//
+//  Created by Lucas Leite on 17/07/20.
+//  Copyright © 2020 Lucas Leite. All rights reserved.
+//
+
+import Combine
+import UIKit
+
+struct CatImageViewModel {
+    private let catImage: CatImage
+    private let repository: CatImageRepository
+
+    var image: AnyPublisher<UIImage?, Never> {
+        repository.fetchImage(for: catImage)
+    }
+
+    init?(catImage: CatImage, repository: CatImageRepository = CatImageRepository()) {
+        guard catImage.link.contains(".jpg") || catImage.link.contains(".png") else {
+            return nil
+        }
+        self.catImage = catImage
+        self.repository = repository
+    }
+}
+
+// MARK: Hashable
+
+extension CatImageViewModel: Hashable {
+    static func == (lhs: CatImageViewModel, rhs: CatImageViewModel) -> Bool {
+        lhs.catImage == rhs.catImage
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(catImage)
+    }
+}
---
+++ Catz/View Model/CatsViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  CatsViewModel.swift
+//  Catz
+//
+//  Created by Lucas Leite on 17/07/20.
+//  Copyright © 2020 Lucas Leite. All rights reserved.
+//
+
+import Combine
+import CombineExt
+import Foundation
+
+final class CatsViewModel {
+    private let repository: CatsRepository
+
+    private var cats: AnyPublisher<[Cat], Never> {
+        repository.fetchCats()
+            .map { $0.data }
+            .share()
+            .eraseToAnyPublisher()
+    }
+
+    private var images: AnyPublisher<[CatImage], Never> {
+        cats
+            .mapMany { $0.images ?? [] }
+            .map { Array($0.joined()) }
+            .eraseToAnyPublisher()
+    }
+
+    var catModels: AnyPublisher<[CatImageViewModel], Never> {
+        images
+            .compactMapMany { CatImageViewModel(catImage: $0) }
+            .eraseToAnyPublisher()
+    }
+
+    init(repository: CatsRepository = CatsRepository()) {
+        self.repository = repository
+    }
+}
